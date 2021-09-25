5b328e59623ca377911219e0b0b3b2b16e070f9b
DaCurse <dacurse0@gmail.com>
2021-09-25 14:38:26 +0300

    Add TodoItem component

---
+++ src/TodoItem.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import type { Todo } from './types/todo.interface';
+
+  export let todo: Todo;
+</script>
+
+<div>
+  <input type="checkbox" bind:checked={todo.completed} />
+  <p class:completed={todo.completed}>{todo.title}</p>
+</div>
+
+<style>
+  p {
+    display: inline-block;
+  }
+
+  .completed {
+    text-decoration: line-through;
+  }
+</style>
