362b614826b90fd0ac0a9b984e98c7f929d4f4a1
DaCurse <dacurse0@gmail.com>
2021-09-25 14:38:37 +0300

    Add TodoList component

---
+++ src/TodoList.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import TodoItem from './TodoItem.svelte';
+  import type { Todo } from './types/todo.interface';
+
+  export let todos: Todo[] = [];
+</script>
+
+{#each todos as todo (todo.id)}
+  <TodoItem {todo} />
+{/each}
