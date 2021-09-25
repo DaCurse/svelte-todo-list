02ae8b253e698ed8473ef873eebe21f01b52af15
DaCurse <dacurse0@gmail.com>
2021-09-25 14:39:39 +0300

    Display TodoList with some dummy items

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,25 @@
 <script lang="ts">
+  import TodoList from './TodoList.svelte';
+  import type { Todo } from './types/todo.interface';
+  const todos: Todo[] = [
+    {
+      id: 1,
+      title: 'Buy eggs',
+      completed: false,
+    },
+    {
+      id: 2,
+      title: 'Buy milk',
+      completed: false,
+    },
+    {
+      id: 3,
+      title: 'Buy bread',
+      completed: false,
+    },
+  ];
 </script>
 
-<main />
+<main>
+  <TodoList {todos} />
+</main>
