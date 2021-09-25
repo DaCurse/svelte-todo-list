d79695a687a6feb21834c7fee9f9ba5962ed4af9
DaCurse <dacurse0@gmail.com>
2021-09-25 15:50:20 +0300

    Added editing to TodoItem

--- src/TodoItem.svelte
+++ src/TodoItem.svelte
@@ -2,19 +2,52 @@
   import type { Todo } from './types/todo.interface';
 
   export let todo: Todo;
+  let isEditing: boolean = false;
+
+  function handleClick() {
+    if (todo.completed) {
+      return;
+    }
+    isEditing = true;
+  }
+
+  function handleBlur() {
+    isEditing = false;
+  }
+
+  function select(el: HTMLInputElement) {
+    el.select();
+  }
 </script>
 
 <div>
   <input type="checkbox" bind:checked={todo.completed} />
-  <p class:completed={todo.completed}>{todo.title}</p>
+  {#if isEditing}
+    <input
+      type="text"
+      bind:value={todo.title}
+      on:blur={handleBlur}
+      use:select
+    />
+  {:else}
+    <div
+      class="content"
+      class:completed={todo.completed}
+      on:click={handleClick}
+    >
+      {todo.title}
+    </div>
+  {/if}
 </div>
 
 <style>
-  p {
+  .content {
+    cursor: pointer;
     display: inline-block;
+    margin-bottom: 2px;
   }
-
-  .completed {
+  .content.completed {
+    cursor: default;
     text-decoration: line-through;
   }
 </style>
